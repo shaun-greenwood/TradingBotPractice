aa674900feea3dfa44fbbc62b1edc8584f71cf9e
shaun-greenwood <84123734+shaun-greenwood@users.noreply.github.com>
2021-06-05 13:08:38 +0100

    proof of connection to API took out the API keys from the .env.  Apparently codesandbox doesnt honour the .gitignore file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-require('dotenv').config();
+require('dotenv').config(); // dotenv uses a seperate file to keep PI keys in
 
-const Alpaca = require('@alpacahq/alpaca-trade-api');
+const Alpaca = require('@alpacahq/alpaca-trade-api'); // get alpaca API
 
+// connect to the API
 const alpaca = new Alpaca({
-  keyId: 'APCA_API_KEY_ID',
-  secretKey: 'APCA_API_SECRET_KEY',
+  keyId: process.env.APCA_API_KEY_ID,
+  secretKey: process.env.APCA_API_SECRET_KEY,
   paper: true,
   usePolygon: false
 });
+
+let accountCash;
+let account;
+const positionSize = 0.01;
+
+//get the account from alpaca
+async function setGlobals(){
+  account = await alpaca.getAccount();
+  accountCash = account.cash;
+  console.log('1 percent of total cash = ' + accountCash*positionSize);
+}
+
+//perform setup
+setGlobals();
+
