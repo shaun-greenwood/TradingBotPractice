e957b5d9d05701120a5853a93ae9388b170e841d
shaun-greenwood <84123734+shaun-greenwood@users.noreply.github.com>
2021-06-07 21:38:31 +0100

    created backtest stream

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config(); // dotenv uses a seperate file to keep PI keys in
+//require('dotenv').config(); // dotenv uses a seperate file to keep PI keys in
+require("technicalindicators"); //library for indicators
 
-const Alpaca = require('@alpacahq/alpaca-trade-api'); // get alpaca API
-const Backtest = require('@kendelchopp/alpaca-js-backtesting');
+const Alpaca = require("@alpacahq/alpaca-trade-api"); // get alpaca API
+const Backtest = require("@kendelchopp/alpaca-js-backtesting");
 
 // connect to the API
 const alpaca = new Alpaca({
@@ -14,21 +15,20 @@ const alpaca = new Alpaca({
 //create instance of back tester on alpaca using Kendel Chopp's code
 const backtest = new Backtest({
   alpaca,
-  startDate: new Date(2020, 1, 1, 0, 0),
-  endDate: new Date(2020, 10, 11, 0, 0)
+  startDate: new Date(2015, 1, 1, 0, 0),
+  endDate: new Date(2021, 1, 1, 0, 0)
 });
 
-let accountCash;
-let account;
-const positionSize = 0.01;
+//open stream
+const client = backtest.data_ws;
+client.onConnect(() => {
+  console.log("opened backtest");
+  client.subscribe(['AM.SPY']);
+});
 
-//get the account from alpaca
-async function setGlobals(){
-  account = await alpaca.getAccount();
-  accountCash = account.cash;
-  console.log('1 percent of total cash = ' + accountCash*positionSize);
-}
+//if the 50 and 200 ema cross over each other, set a variable to long or short
 
-//perform setup
-setGlobals();
+//if Long, and the price drops below the 50, but above the 200:
+// if the next candle closes above the 50, go long
 
+client.disconnect();
